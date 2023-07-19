42b83727504412de4e625d67793cd41933a9ef04
Toxic <105833255+DevinMina@users.noreply.github.com>
2023-07-19 20:52:41 +0000

    FINAL UPDATE

--- DevinMinaC868/Appt/EditAppt.Designer.cs
+++ DevinMinaC868/Appt/EditAppt.Designer.cs
@@ -50,26 +50,23 @@ namespace DevinMinaC868.Appt
             this.appointmentText = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.customerComboBox = new System.Windows.Forms.ComboBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(564, 57);
-            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Location = new System.Drawing.Point(423, 46);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(123, 16);
+            this.label10.Size = new System.Drawing.Size(99, 13);
             this.label10.TabIndex = 84;
             this.label10.Text = "Select Appointment";
             // 
             // appointmentComboBox
             // 
             this.appointmentComboBox.FormattingEnabled = true;
-            this.appointmentComboBox.Location = new System.Drawing.Point(717, 57);
-            this.appointmentComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.appointmentComboBox.Location = new System.Drawing.Point(538, 46);
             this.appointmentComboBox.Name = "appointmentComboBox";
-            this.appointmentComboBox.Size = new System.Drawing.Size(345, 24);
+            this.appointmentComboBox.Size = new System.Drawing.Size(260, 21);
             this.appointmentComboBox.TabIndex = 83;
             this.appointmentComboBox.Text = "--Select--";
             this.appointmentComboBox.SelectedValueChanged += new System.EventHandler(this.AppointmentComboBox_SelectedValueChanged);
@@ -84,10 +81,9 @@ namespace DevinMinaC868.Appt
             "Consultation",
             "Auditing",
             "Other"});
-            this.typeComboBox.Location = new System.Drawing.Point(175, 155);
-            this.typeComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.typeComboBox.Location = new System.Drawing.Point(131, 126);
             this.typeComboBox.Name = "typeComboBox";
-            this.typeComboBox.Size = new System.Drawing.Size(345, 24);
+            this.typeComboBox.Size = new System.Drawing.Size(260, 21);
             this.typeComboBox.TabIndex = 82;
             // 
             // startDateValue
@@ -95,10 +91,9 @@ namespace DevinMinaC868.Appt
             this.startDateValue.Checked = false;
             this.startDateValue.CustomFormat = "MM/dd/yyyy hh:mm tt";
             this.startDateValue.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.startDateValue.Location = new System.Drawing.Point(717, 123);
-            this.startDateValue.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.startDateValue.Location = new System.Drawing.Point(538, 100);
             this.startDateValue.Name = "startDateValue";
-            this.startDateValue.Size = new System.Drawing.Size(345, 22);
+            this.startDateValue.Size = new System.Drawing.Size(260, 20);
             this.startDateValue.TabIndex = 81;
             // 
             // endDateValue
@@ -106,19 +101,17 @@ namespace DevinMinaC868.Appt
             this.endDateValue.Checked = false;
             this.endDateValue.CustomFormat = "MM/dd/yyyy hh:mm tt";
             this.endDateValue.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.endDateValue.Location = new System.Drawing.Point(717, 155);
-            this.endDateValue.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.endDateValue.Location = new System.Drawing.Point(538, 126);
             this.endDateValue.Name = "endDateValue";
-            this.endDateValue.Size = new System.Drawing.Size(345, 22);
+            this.endDateValue.Size = new System.Drawing.Size(260, 20);
             this.endDateValue.TabIndex = 80;
             // 
             // cancelButton
             // 
             this.cancelButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.cancelButton.Location = new System.Drawing.Point(964, 374);
-            this.cancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cancelButton.Location = new System.Drawing.Point(723, 304);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(100, 28);
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 79;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = false;
@@ -127,10 +120,9 @@ namespace DevinMinaC868.Appt
             // updateButton
             // 
             this.updateButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.updateButton.Location = new System.Drawing.Point(25, 368);
-            this.updateButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.updateButton.Location = new System.Drawing.Point(19, 299);
             this.updateButton.Name = "updateButton";
-            this.updateButton.Size = new System.Drawing.Size(100, 28);
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
             this.updateButton.TabIndex = 78;
             this.updateButton.Text = "Update";
             this.updateButton.UseVisualStyleBackColor = false;
@@ -139,153 +131,128 @@ namespace DevinMinaC868.Appt
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(21, 21);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Location = new System.Drawing.Point(16, 17);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(428, 16);
+            this.label9.Size = new System.Drawing.Size(343, 13);
             this.label9.TabIndex = 77;
             this.label9.Text = "Please select the customer you would like to modify an appointment for.";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(564, 155);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(423, 126);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(65, 16);
+            this.label8.Size = new System.Drawing.Size(52, 13);
             this.label8.TabIndex = 76;
             this.label8.Text = "End Time";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(564, 123);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(423, 100);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(68, 16);
+            this.label7.Size = new System.Drawing.Size(55, 13);
             this.label7.TabIndex = 75;
             this.label7.Text = "Start Time";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(21, 156);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(16, 127);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(39, 16);
+            this.label6.Size = new System.Drawing.Size(31, 13);
             this.label6.TabIndex = 74;
             this.label6.Text = "Type";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(21, 123);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(16, 100);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 16);
+            this.label5.Size = new System.Drawing.Size(44, 13);
             this.label5.TabIndex = 73;
             this.label5.Text = "Contact";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(21, 90);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(16, 73);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 16);
+            this.label4.Size = new System.Drawing.Size(48, 13);
             this.label4.TabIndex = 72;
             this.label4.Text = "Location";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 204);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(16, 166);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.Size = new System.Drawing.Size(60, 13);
             this.label3.TabIndex = 71;
             this.label3.Text = "Description";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(564, 89);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(423, 72);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 16);
+            this.label2.Size = new System.Drawing.Size(97, 13);
             this.label2.TabIndex = 70;
             this.label2.Text = "Appointment Name";
             // 
             // contactText
             // 
-            this.contactText.Location = new System.Drawing.Point(175, 123);
-            this.contactText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.contactText.Location = new System.Drawing.Point(131, 100);
             this.contactText.Name = "contactText";
-            this.contactText.Size = new System.Drawing.Size(345, 22);
+            this.contactText.Size = new System.Drawing.Size(260, 20);
             this.contactText.TabIndex = 69;
             // 
             // locationText
             // 
-            this.locationText.Location = new System.Drawing.Point(175, 90);
-            this.locationText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.locationText.Location = new System.Drawing.Point(131, 73);
             this.locationText.Name = "locationText";
-            this.locationText.Size = new System.Drawing.Size(345, 22);
+            this.locationText.Size = new System.Drawing.Size(260, 20);
             this.locationText.TabIndex = 68;
             // 
             // descriptionText
             // 
-            this.descriptionText.Location = new System.Drawing.Point(175, 201);
-            this.descriptionText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.descriptionText.Location = new System.Drawing.Point(131, 163);
             this.descriptionText.Multiline = true;
             this.descriptionText.Name = "descriptionText";
-            this.descriptionText.Size = new System.Drawing.Size(888, 137);
+            this.descriptionText.Size = new System.Drawing.Size(667, 112);
             this.descriptionText.TabIndex = 67;
             // 
             // appointmentText
             // 
-            this.appointmentText.Location = new System.Drawing.Point(717, 89);
-            this.appointmentText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.appointmentText.Location = new System.Drawing.Point(538, 72);
             this.appointmentText.Name = "appointmentText";
-            this.appointmentText.Size = new System.Drawing.Size(345, 22);
+            this.appointmentText.Size = new System.Drawing.Size(260, 20);
             this.appointmentText.TabIndex = 66;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 57);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(16, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 16);
+            this.label1.Size = new System.Drawing.Size(84, 13);
             this.label1.TabIndex = 65;
             this.label1.Text = "Select Customer";
             // 
             // customerComboBox
             // 
             this.customerComboBox.FormattingEnabled = true;
-            this.customerComboBox.Location = new System.Drawing.Point(175, 57);
-            this.customerComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.customerComboBox.Location = new System.Drawing.Point(131, 46);
             this.customerComboBox.Name = "customerComboBox";
-            this.customerComboBox.Size = new System.Drawing.Size(345, 24);
+            this.customerComboBox.Size = new System.Drawing.Size(260, 21);
             this.customerComboBox.TabIndex = 64;
             this.customerComboBox.Text = "--Select--";
             this.customerComboBox.SelectedValueChanged += new System.EventHandler(this.CustomerComboBox_SelectedValueChanged);
             // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(660, 0);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(95, 20);
-            this.checkBox1.TabIndex = 85;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
             // EditAppt
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1091, 417);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(818, 339);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.appointmentComboBox);
             this.Controls.Add(this.typeComboBox);
@@ -307,7 +274,6 @@ namespace DevinMinaC868.Appt
             this.Controls.Add(this.appointmentText);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.customerComboBox);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "EditAppt";
             this.Text = "EditAppt";
             this.ResumeLayout(false);
@@ -338,6 +304,5 @@ namespace DevinMinaC868.Appt
         private System.Windows.Forms.TextBox appointmentText;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox customerComboBox;
-        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
